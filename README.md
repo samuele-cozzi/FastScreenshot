c4d2ea7e721e8fd69044a62606ce0445a52dd635
Samuele Cozzi <samuele.cozzi@soprasteria.com>
2018-08-17 17:47:54 +0200

    program

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # FastScreenshot
+
+## Introduction
 Console application for take screenshot and save it local
+
+## Coding
+
+### Prerequisites
+- git
+- dotnet core
+
+### Prepare
+```shell
+$ git clone https://github.com/samuele-cozzi/FastScreenshot.git
+$ code .\FastScreenshot\
+```
+
+```shell
+$ dotnet restore source
+```
+
+### Execute
+
+```shell
+$ dotnet build source
+$ cd .\source\
+$ dotnet run
+```
+
+### Delivery
+```shell
+$ dotnet publish
+```
+
+## References
+
+- [Sample](https://code.msdn.microsoft.com/windowsdesktop/Saving-a-screenshot-using-C-6883abb3)
+- [Drawing Common](https://developers.de/2018/01/22/how-to-use-system-drawing-in-net-core/)
+
+## License
+[MIT License](LICENSE)
--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
 namespace source
 {
@@ -6,7 +12,58 @@ namespace source
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+                Console.WriteLine(keyinfo.Key.ToString() + " was pressed");
+                Console.WriteLine(keyinfo.Modifiers.ToString() + " was pressed");
+                if (keyinfo.Key.ToString() == "P" && keyinfo.Modifiers.ToString() == "Shift"){
+                    print_screen();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+            while (keyinfo.Key != ConsoleKey.X);
+        }
+
+        static void print_screen(){
+            // Start the process... 
+            Console.WriteLine("Initializing the variables...");
+            Console.WriteLine();
+            Bitmap memoryImage;
+            memoryImage = new Bitmap(1366, 800);
+            Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+            // Create graphics 
+            Console.WriteLine("Creating Graphics...");
+            Console.WriteLine();
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+            // Copy data from screen 
+            Console.WriteLine("Copying data from screen...");
+            Console.WriteLine();
+            memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+            //That's it! Save the image in the directory and this will work like charm. 
+            string str = "";
+            try
+            {
+                str = string.Format(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) +
+                      @"\Screenshots\" + DateTime.Now.Ticks.ToString() + "_Screenshot.png");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine("Sorry, there was an error: " + er.Message);
+                Console.WriteLine();
+            }
+
+            // Save it! 
+            Console.WriteLine("Saving the image...");
+            memoryImage.Save(str);
+
+            // Write the message, 
+            Console.WriteLine("Picture has been saved...");
+            Console.WriteLine();
         }
     }
 }
